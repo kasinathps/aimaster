48c09db9342a40b38608a526bc61a191e4afb742
kasinathps <bladey@localhost.localdomain>
2018-10-16 20:27:33 +0530

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
   url='https://github.com/kasinathps/aimaster',
   author='Kasinath P.S',
   author_email='kasinathps@gmail.com',
-  packages=['aimaster','aimaster.nn1hlib','aimaster.nn1hlnb'],
+  packages=setuptools.find_packages(),
   install_requires=['numpy','scipy'],
   version='1.0.1',
   license='MIT',
