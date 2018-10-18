4c29463cb82fd042067647563c17e8c106d9975c
kasinathps <bladey@localhost.localdomain>
2018-10-18 12:51:32 +0530

    v1.0.5

--- aimaster/nn1hlib/nn1hlib.py
+++ aimaster/nn1hlib/nn1hlib.py
@@ -2,17 +2,15 @@
 #make input matrix as 'x' and output matrix as 'y'
 import numpy as np
 from scipy.special import expit
-def createnn(inputsize,hiddenlayersize,outputsize,x,pt=True):
+def createnn(inputsize,hiddenlayersize,outputsize,pt=True):
   '''creates weight matrices w1 and w2 with appropriate weights
   according to inputsize,hiddenlayersize and outputsize
   if pt is set true (true by default) the function prints weight matrices 
   at the end , else if pt is set false it wont
-
-  NOTE: give input matrix as 'x' , biased input matrix X will be created
-  by the function itself!'''
-  global w1,w2,X
-  X=np.ones([x.shape[0],1])
-  X=np.append(X,x,axis=1)
+  
+  NOTE: give input size as the real input size without accounting bias.
+  bias weights will be added automatically here!'''
+  global w1,w2
   w1=np.random.rand(hiddenlayersize,inputsize+1)
   w2=np.random.rand(outputsize,hiddenlayersize)
   if pt:
@@ -37,7 +35,7 @@ def predict(x,add_bias=0):
     x=np.append(np.ones([x.shape[0],1]),x,axis=1)
   return expit(np.matmul(expit(np.matmul(x,w1.T)),w2.T))
 
-def train(X,y,iterations,learningrate=0.1,printy=True,printw=True):
+def train(x,y,iterations,learningrate=0.1,printy=True,printw=True):
   
   '''over the iterations, this function optimizes the values of w1 and
    w2 to reduce output error.
@@ -48,8 +46,13 @@ def train(X,y,iterations,learningrate=0.1,printy=True,printw=True):
   NOTE: learning rate is set default to 0.1 which sometimes is
   morethan required (result: gradient descent will not converge) or less 
   than required (result: slow training). feel free to experiment with 
-  different values as this module is for basic understanding :) '''
+  different values as this module is for basic understanding :)
+  
+  NOTE: give input matrix as 'x' , biased input matrix X will be created
+  by the function itself!'''
   global w1,w2
+  X=np.ones([x.shape[0],1])
+  X=np.append(X,x,axis=1)
   for j in range(iterations):
     for i in range(len(X)):
       Hsum=np.matmul(X[i],w1.T)
