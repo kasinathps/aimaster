905f771b241092288ca01d679a821b8bc0bd74cd
kasinathps <bladey@localhost.localdomain>
2018-11-01 11:06:47 +0530

    nnplotter tool added

---
+++ aimaster/tools/nnplotter.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+def plotinit():
+    global ax
+    fig,ax=plt.subplots()
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
+
+def plot(p,parameter,weight=1):
+    global ax
+    try :
+        assert ax
+    except:
+        print('plot not initialized')
+        return
+    if weight>0:
+        ax.plot([parameter,parameter+1],[p[1],p[0]],linewidth=weight)
+    elif weight<0:
+        ax.plot([parameter,parameter+1],[p[1],p[0]],dashes=[4,2],linewidth=abs(weight))
+
+def plotweights(w,parameter):
+    for i in np.indices(w.shape).reshape(2,w.shape[0]*w.shape[1]).T:
+        plot(i,parameter,w[tuple(i)])
